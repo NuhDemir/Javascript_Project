9c04b67ba95aa2d645d14109eb65a609e6d2be73
NuhDemir <115398614+NuhDemir@users.noreply.github.com>
2024-02-26 00:01:25 +0300

    new

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,16 +1,17 @@
 document.querySelector('#push').onclick = function() {
 
-    if (document.querySelector('#newtask input').nodeValue.lenght == 0) {
+    if (document.querySelector('#newtask input').value.lenght == 0) {
         alert("Please Enter a task")
     }
     else{
-        document.querySelector('tasks').innerHTML+= `
+        document.querySelector('tasks').innerHTML
+        += `
         <div class="task">
             <span id="taskname">
             ${document.querySelector
             ('newtask input').value}
        </span>
-
+        <button class = "delete">
     </div> 
     `;
     } 
