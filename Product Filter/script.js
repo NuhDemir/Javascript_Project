e9da3b0abee75a87cadf665f383ead69963c9def
NuhDemir <115398614+NuhDemir@users.noreply.github.com>
2024-02-29 23:34:12 +0300

    new

---
+++ Product Filter/script.js
@@ -0,0 +1,28 @@
+let products = {
+    data: [ 
+    {
+        productName:"Regular White T-shirt",
+        category: "TopWear",
+        price:"30",
+        image: "White-Tshirt.jpg"
+    },
+    {
+        productName:"Beige White T-shirt",
+        category: "BottomWear",
+        price:"32",
+        image: "beige-Tshirt.jpg"
+    },
+    {
+        productName:"Regular Black T-shirt",
+        category: "TopWear",
+        price:"30",
+        image: "Black-Tshirt.jpg"
+    }
+    {
+        productName:"Regular White T-shirt",
+        category: "TopWear",
+        price:"30",
+        image: "White-Tshirt.jpg"
+    }
+]
+};
