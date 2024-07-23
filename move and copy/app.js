e005780372b2abb786ff009b8fab47920bc34a9c
NuhDemir <drmaa217@gmail.com>
2024-07-23 22:04:59 +0300

    copy and move js

---
+++ move and copy/app.js
@@ -0,0 +1,5 @@
+const copyText = document.querySelector("textarea[name=copyTxt");
+const finalText = document.querySelector("textarea[name=finalTxt]");
+const copyBtn = document.querySelector(".copyBtn");
+const copyBtn = document.querySelector(".copyBtn");
+const output = document.querySelector(".output");
